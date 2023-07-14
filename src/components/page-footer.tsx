c43c4956269cad584aa1d47b337438db60bf4685
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-07-13 20:38:03 -0700

    fix: add links to header/footer

--- src/components/page-footer.tsx
+++ src/components/page-footer.tsx
@@ -1,14 +1,40 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading } from "@chakra-ui/react";
 import OneBtcLogo from "./1btc-logo";
 
 function Footer() {
   return (
     <Flex align="center">
       <OneBtcLogo width="75px" height="75px" />
-      <Flex flex="1" ml={2} align="center">
+      <Flex flexGrow="1" ml={2} align="center">
         <Heading>1btc</Heading>
       </Flex>
-      <Text justifySelf="flex-end">Footer links can go here</Text>
+      <Button
+        as="a"
+        variant="ghost"
+        href="https://docs.1btc.chat/1btc-chat-api"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        API
+      </Button>
+      <Button
+        as="a"
+        variant="ghost"
+        href="https://github.com/1btc-news"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </Button>
+      <Button
+        as="a"
+        variant="ghost"
+        href="https://www.twitter.com/1btcnews"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </Button>
     </Flex>
   );
 }
--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,14 +1,31 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading } from "@chakra-ui/react";
 import OneBtcLogo from "./1btc-logo";
 
 function Header() {
   return (
     <Flex align="center">
       <OneBtcLogo width="75px" height="75px" />
-      <Flex flex="1" ml={2} align="center">
+      <Flex flexGrow="1" ml={2} align="center">
         <Heading>1btc</Heading>
       </Flex>
-      <Text justifySelf="flex-end">Header links can go here</Text>
+      <Button
+        as="a"
+        variant="ghost"
+        href="https://1btc.chat"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        About
+      </Button>
+      <Button
+        as="a"
+        variant="ghost"
+        href="https://docs.1btc.chat"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Docs
+      </Button>
     </Flex>
   );
 }
