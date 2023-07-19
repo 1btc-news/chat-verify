0a4113e58965b1f2f9dae7caf9d64ba708dade8e
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-07-19 14:51:40 -0700

    feat(page-content): refactor page content component
    Changes Made:
    - Removed unused imports
    - Refactored account data loading logic
    - Updated error handling
    
    New Additions:
    - Added account-data hook for fetching account data

--- src/components/page-content.tsx
+++ src/components/page-content.tsx
@@ -14,23 +14,18 @@ import {
 } from "@chakra-ui/react";
 import { useAccount } from "@micro-stacks/react";
 import { useAtom } from "jotai";
-import { loadable } from "jotai/utils";
-import {
-  fetchAccountDataAtom,
-  storedStxAddressAtom,
-  storedUserDataAtom,
-} from "../constants";
+import { storedStxAddressAtom, storedUserDataAtom } from "../constants";
 import ConnectWallet from "./connect-wallet";
 import SignOut from "./sign-out";
 import VerificationFlow from "./verification-flow";
 import ClearData from "./clear-data";
+import { useAccountData } from "../hooks/account-data";
 
 function Content() {
   const { stxAddress } = useAccount();
   const [storedStxAddress] = useAtom(storedStxAddressAtom);
   const [storedUserData] = useAtom(storedUserDataAtom);
-  const accountDataLoader = loadable(fetchAccountDataAtom);
-  const [accountData] = useAtom(accountDataLoader);
+  const { isLoading, hasError, hasData, error } = useAccountData();
 
   // if user is not logged in already
   if (!stxAddress || !storedStxAddress || !storedUserData) {
@@ -83,19 +78,19 @@ function Content() {
   // if user is logged in, check if account data is loaded
   return (
     <Box width="100%" maxW="1200px">
-      {accountData.state === "loading" && (
+      {isLoading && (
         <>
           <Spinner color="orange.500" emptyColor="orange.200" />
           <Text>Loading account data...</Text>
         </>
       )}
-      {accountData.state === "hasError" && (
+      {hasError && (
         <>
           <Text>There was an error, please sign out and try again.</Text>
-          <Text>{String(accountData.error)}</Text>
+          <Text>{String(error)}</Text>
         </>
       )}
-      {accountData.state === "hasData" && <VerificationFlow />}
+      {hasData && <VerificationFlow />}
       <Stack direction="row" justifyContent="flex-end">
         <ClearData />
         <SignOut />
